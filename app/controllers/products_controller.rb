d18ba3c459b5833cc85466d7ce67b2793f8ea4f2
hina <oor_ymsthn@icloud.com>
2021-06-05 16:47:15 +0900

    ユーザー管理機能

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,7 @@
+class ProductsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  
+  def index
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :DeviseCreateUsers
-  root to; 'products#index'
+  devise_for :users
+  root to: 'products#index'
 end
