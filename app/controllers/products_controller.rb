7b8c6191a4b5f2ed6f6a90c0069c0bb319d9d4a7
hina <oor_ymsthn@icloud.com>
2021-06-05 18:46:17 +0900

    change2

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
+  # before_action :authenticate_user!, except: [:index, :show]
 
-  def index
-  end
+  # def index
+  # end
 end
