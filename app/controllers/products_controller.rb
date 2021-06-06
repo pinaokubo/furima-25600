b40a3aa3950aac829c0b0e9f6de04430ad249456
hina <oor_ymsthn@icloud.com>
2021-06-06 17:11:19 +0900

    編集

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,7 +2,7 @@ class ProductsController < ApplicationController
   # before_action :authenticate_user!, except: [:index, :show]
 
   def index
-    @products = Product.order("create_at DESC")
+    @products = Product.all
   end
 
   def new
