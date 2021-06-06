f7ab53b4120e8055f92c324a46d86e85047974eb
hina <oor_ymsthn@icloud.com>
2021-06-06 11:56:51 +0900

    変更

--- app/models/product.rb
+++ app/models/product.rb
@@ -17,18 +17,14 @@ class Product < ApplicationRecord
     validates :price,
               numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999,
                               message: 'is invalid' }
-    validates :category_id
-    validates :status_id
-    validates :shipping_fee_id
-    validates :prefecture_id
-    validates :scheduled_delivery_id
   end
-
-  with_options numericality: { other_than: 0, message: 'is invalid' } do
-    validates :category_id
-    validates :status_id
-    validates :shipping_fee_id
-    validates :prefecture_id
-    validates :scheduled_delivery_id
-  end
+   with_options presence: true do
+     with_options numericality: { other_than: 0, message: 'is invalid' } do
+       validates :category_id
+       validates :status_id
+       validates :shipping_fee_id
+       validates :prefecture_id
+       validates :scheduled_delivery_id
+     end
+   end
 end
