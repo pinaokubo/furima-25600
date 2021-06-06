658ca8dd0e4268218a44301f506571fd7c6c433f
hina <oor_ymsthn@icloud.com>
2021-06-06 12:33:43 +0900

    みやすくしました汗

--- app/models/product.rb
+++ app/models/product.rb
@@ -10,19 +10,19 @@ class Product < ApplicationRecord
   belongs_to :scheduled_delivery
   belongs_to :user
 
-   with_options presence: true do
-    validates :image
-    validates :name
-    validates :info
-    validates :price,
+    with_options presence: true do
+       validates :image
+       validates :name
+       validates :info
+       validates :price,
               numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999,
                               message: 'is invalid' }
-     with_options numericality: { other_than: 0, message: 'is invalid' } do
-       validates :category_id
-       validates :status_id
-       validates :shipping_fee_id
-       validates :prefecture_id
-       validates :scheduled_delivery_id
-     end
-   end
+       with_options numericality: { other_than: 0, message: 'is invalid' } do
+          validates :category_id
+          validates :status_id
+          validates :shipping_fee_id
+          validates :prefecture_id
+           validates :scheduled_delivery_id
+        end
+    end
 end
