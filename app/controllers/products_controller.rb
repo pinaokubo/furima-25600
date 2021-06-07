926c62bf4349f99de391585d35bc6a3ea8442547
hina <oor_ymsthn@icloud.com>
2021-06-07 11:17:57 +0900

    導入

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,7 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_product, only: [:show, :edit, :update]
-  before_action :unless_user, only: [:edit, :update]
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
+  before_action :unless_user, only: [:edit, :update, :destroy]
 
   def index
     @products = Product.order("created_at DESC")
@@ -33,6 +33,11 @@ class ProductsController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def destroy
+    @product.destroy
+    redirect_to root_path
   end
 
   private
